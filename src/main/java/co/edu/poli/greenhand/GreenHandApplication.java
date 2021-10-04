e309f3869b516d337a459edb2ac855f28f53e4df
Jeison Angel <jsangel@poligran.edu.co>
2021-10-03 20:14:04 -0500

    Inicialización de proyecto
    - Se crea el proyecto green_hand
    - Se añaden dependencias de Java Mailer, MongoDB y Spring Web
    - Se añaden dependencias de Swagger
    - Se modifica el archivo SwaggerConfig.java según la idea del proyecto

---
+++ src/main/java/co/edu/poli/greenhand/GreenHandApplication.java
@@ -0,0 +1,13 @@
+package co.edu.poli.greenhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenHandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenHandApplication.class, args);
+	}
+
+}
